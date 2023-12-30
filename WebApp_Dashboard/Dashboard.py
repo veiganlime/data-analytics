694a1c16177710d135f77f014919ff06e37bffd5
lime <lime@limelime.local>
2023-12-30 10:40:07 +0100

    minor changes and implementation of allocation pie-chart

--- WebApp_Dashboard/Dashboard.py
+++ WebApp_Dashboard/Dashboard.py
@@ -4,6 +4,7 @@ import numpy as np
 import sqlite3 as sql
 import main
 import yfinance as yf
+import plotly_express as px
 
 st.sidebar.write("Select dashboard:")
 
@@ -15,35 +16,33 @@ option = st.sidebar.selectbox(
 
 if option == "Porfolio owerview":
 
-    st.title("Portfolio overwiev")
+    st.title("Portfolio overwiev")    
 
-    conn = sql.connect('data/Crypto.db')    # SQL database connection
-    df = pd.read_sql_query("SELECT * FROM PORTFOLIO", conn)
-    df = df.drop(columns=['ID'])
-    conn = sql.connect('data/Crypto.db')
-
-
+    conn = sql.connect('data/Crypto.db') # SQL database connection
     df_sum = pd.read_sql_query("SELECT * FROM PORTFOLIO", conn)
     df_sum = df_sum.drop(columns=['ID'])
 
 
     df_sum['TotalAmount'] = df_sum.groupby('Ticker')['Amount'].transform('sum')
+    df_sum.rename(columns={'TotalAmount':'Quantity'}, inplace=True)
 
     pd.set_option('display.float_format', '{:.6f}'.format)
-    df_sum_1 = df_sum[['Ticker', 'TotalAmount']].drop_duplicates()
+    df_prepeared = df_sum[['Ticker', 'Quantity']].drop_duplicates()
     tickers_to_drop = ['VTX', 'CITY', 'IONX']# Drop a few rows for now due to an issue with the market maker. In the future, try using API requests with another financial aggregator.
-    df_sum_1 = df_sum_1[~df_sum_1['Ticker'].isin(tickers_to_drop)]
-    values = []
+    df_prepeared = df_prepeared[~df_prepeared['Ticker'].isin(tickers_to_drop)]
 
-    for index, row in df_sum_1.iterrows(): # Loop to calculate the current value of each coin.
+    values = []
+    for index, row in df_prepeared.iterrows(): # Loop to calculate the current value of each coin.
         ticker = row['Ticker']
-        total_amount = row['TotalAmount']
+        total_amount = row['Quantity']
         price = main.get_price(ticker)# get_price function with API request.
         calculated_value = total_amount * price
         values.append(calculated_value)
 
-    df_sum_1['value'] = values
-    st.dataframe(df_sum_1)
+    df_prepeared['value'] = values
+    st.dataframe(df_prepeared)
+    plot = px.pie(df_prepeared, values='value', names='Ticker',title='Allocation',width=650, height=650 )
+    st.plotly_chart(plot)
 
 
 if option == "Line chart":
@@ -54,14 +53,20 @@ if option == "Line chart":
         start = st.date_input('Start', value = pd.to_datetime('2023-12-01'))
         end = st.date_input('End', value = pd.to_datetime('today'))
         df = yf.download(ticker, start, end)
-        df = df.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'])
+        df = df.drop(columns=['Open', 'High', 'Low', 'Adj Close'])
 
         df.reset_index(inplace=True)
+        df.rename(columns={'Close':'Price'}, inplace=True)
         df['Date'] = df['Date'].dt.strftime('%Y/%m/%d')    
 
         st.line_chart(
-        df, x="Date", y=["Close"], color=["#FF0000"] 
+        df, x="Date", y=["Price"], color=["#FF0000"] 
         )
+        st.line_chart(
+        df, x="Date", y=["Volume"], color=["#0000FF"] 
+        )
+    else:
+        st.write("Please enter the tickers value")
 
 
 if option == "dashboard 3":
